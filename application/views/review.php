9d3b3e559d8b892e550c0dfe942c276ddcc2128e
beladinaelfitri <beladinaelfitri@gmail.com>
2019-04-23 22:28:12 +0700

    ada yang di update

--- application/views/review.php
+++ application/views/review.php
@@ -575,8 +575,8 @@
                 </div>
             </div>
         </footer>
-        <script type="text/javascript" src="assets/js/jquery.min.js"></script>
+        <script type="text/javascript" src="<?php echo base_url().'assets/js/jquery.min.js'?>"></script>
+        <script type="text/javascript" src="<?php echo base_url().'assets/js/bootstrap.min.js'?>"></script> 
         <script type="text/javascript" src="assets/js/popper.min.js"></script> 
-        <script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
     </body>
 </html>
